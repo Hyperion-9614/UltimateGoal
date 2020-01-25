7722a4d806de71afbf34e9824346762785297b3a
Aditya Mangalampalli <aditya.mangalampalli@gmail.com>
2020-01-24 19:03:56 -0800

    POST SC Vision Commit
    Signed-off-by: Aditya Mangalampalli <aditya.mangalampalli@gmail.com>

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Vision.java
@@ -7,17 +7,27 @@ import org.firstinspires.ftc.teamcode.core.Hardware;
 @TeleOp
 public class Vision extends LinearOpMode {
     private Hardware hardware;
-    public static int[] skystonePositions;
+    private int[] skystonePositions;
 
     @Override
     public void runOpMode() {
         hardware = new Hardware(this);
         hardware.initCV();
         waitForStart();
-        while (opModeIsActive()){
-            skystonePositions = hardware.cvPipeline.getSkystonePositions(0);
-            System.out.println("The Skystones are located at:" + skystonePositions[0] + "and " + skystonePositions[1]);
+        while (opModeIsActive() && hardware.cvPipeline.getPipelineActive()) {
+            if (hardware.cvPipeline.getPipelineActive()) {
+                hardware.cvPipeline.setPipelineActive(false);
+                skystonePositions = hardware.cvPipeline.getSkystonePositions(0);
+//                if (hardware.opModeID.contains("blue")) {
+//                    skystonePositions = hardware.cvPipeline.getSkystonePositions(5);
+//                } else if (hardware.opModeID.contains("red")) {
+//                    skystonePositions = hardware.cvPipeline.getSkystonePositions(0);
+//                }
+                telemetry.addLine("The Skystones are located at:" + skystonePositions[0] + "and " + skystonePositions[1]);
+                telemetry.update();
+                hardware.killCV();
+                break;
+            }
         }
-
     }
 }
